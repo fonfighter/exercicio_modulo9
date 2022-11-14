71352fd9cec269dedff3556ffcdadcc3d505eb2e
Rhuan Barbosa <rhuan.b.santos@hotmail.com>
2022-11-14 18:23:26 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
 const form = document.getElementById('form-tarefa');
-let linhas = '';
+
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     const inputNomeTarefa = document.getElementById('nome-tarefa');
-
-    let linha = '<tr>';
-    linha += `<td>${inputNomeTarefa.value}</td>`;
-    linha += '</tr>';
-
-    linhas += linha;
-
     const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
+    const novaTarefa = $('<td></td>');
+    $(`inputNomeTarefa`).appendTo(novaTarefa);
 
-    $('.task').click(function() {
+    $(novaTarefa).click(function() {
 
-        $(td).css('text-decoration', 'line-through');
+        $(this).css('text-decoration', 'line-through');
 
     })
 
